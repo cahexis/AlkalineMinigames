71331970f562a9c5b206e975a8edd9010efa39cd
Emi <183315760+cahexis@users.noreply.github.com>
2026-01-16 11:50:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-BOT_TOKEN = 'BOT TOKEN HERE'
-
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}')
-
+BOT_TOKEN = 'HIDDEN TOKEN LOL!!!'
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-client = Client(intents=intents)
-client.run(BOT_TOKEN)
+
+
+@bot.event
+async def on_guild_join(guild):
+    print(f"Joined server named: {guild.name}")
+
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}')
+    await bot.tree.sync()
+    print("Commands ready")
+
+@bot.tree.command(name="bet", description="Bet on your next coin flip.")
+@app_commands.describe(amount_to_bet = "how much would you like to bet?")
+async def say(interaction: discord.Integration, amount_to_bet: int):
+    await interaction.response.send_message("GO AWAY")
+
+@bot.tree.command(name="flip", description="Flips the coin.")
+@app_commands.describe(choose_side = "Type in the side you would like to bet on")
+async def say(interaction: discord.Integration, choose_side: str):
+    await interaction.response.send_message(f"you bet on {choose_side}")
+
+bot.run(BOT_TOKEN)
 
